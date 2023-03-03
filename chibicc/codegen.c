e05ecc273ea952099a0c1700ae929cbd47c9dcea
gongqi.zhen <gongqi.zhen@gmail.com>
2023-03-04 00:05:18 +0900

    Add a representative node to each Node to improve error messages

--- chibicc/codegen.c
+++ chibicc/codegen.c
@@ -31,7 +31,7 @@ static void gen_addr(Node *node) {
         return;
     }
 
-    error("not an lvalue");
+    error_tok(node->tok, "not an lvalue");
 }
 
 // Generate code for a given node.
@@ -94,7 +94,7 @@ static void gen_expr(Node *node) {
             printf(" movzb %%al, %%rax\n");
             return;
     }
-    error("invalid expression");
+    error_tok(node->tok, "invalid expression");
 }
 
 static void gen_stmt(Node *node) {
@@ -142,7 +142,7 @@ static void gen_stmt(Node *node) {
             return;
     }
 
-    error("invalid statement");
+    error_tok(node->tok, "invalid statement");
 }
 
 // Assign offsets to local variables.
