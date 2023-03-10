92986484a79b934846333c92f466f9b0a980bcff
gongqi.zhen <gongqi.zhen@gmail.com>
2023-03-10 09:39:11 +0900

    Add struct

--- chibicc/chibicc.h
+++ chibicc/chibicc.h
@@ -10,6 +10,7 @@
 
 typedef struct Type Type;
 typedef struct Node Node;
+typedef struct Member Member;
 
 //
 // strings.c
@@ -93,6 +94,7 @@ typedef enum {
     ND_LE,          // <=
     ND_ASSIGN,      // =
     ND_COMMA,       // ,
+    ND_MEMBER,      // . (struct member access)
     ND_ADDR,        // unary &
     ND_DEREF,       // unary *
     ND_RETURN,      // "return"
@@ -126,6 +128,9 @@ struct Node {
     // Block or statement expression
     Node *body;
 
+    // Struct member access
+    Member *member;
+
     // Function call
     char *funcname;
     Node *args;
@@ -145,6 +150,7 @@ typedef enum {
     TY_PTR,
     TY_FUNC,
     TY_ARRAY,
+    TY_STRUCT,
 } TypeKind;
 
 struct Type {
@@ -168,10 +174,21 @@ struct Type {
     // Array
     int array_len;
 
+    // Struct
+    Member *members;
+
     // Function type
     Type *return_ty;
     Type *params;
     Type *next;
+};
+
+// Struct member
+struct Member {
+    Member *next;
+    Type *ty;
+    Token *name;
+    int offset;
 };
 
 extern Type *ty_char;
