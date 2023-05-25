a463196cd6fe25432248dce674c129c03e0533de
gongqi.zhen <gongqi.zhen@gmail.com>
2023-05-26 08:58:18 +0900

    Add the null directive

--- chibicc/tokenize.c
+++ chibicc/tokenize.c
@@ -6,6 +6,9 @@ static char *current_filename;
 // Input string
 static char *current_input;
 
+// True if the current position is at the beginning of a line
+static bool at_bol;
+
 // Reports an error and exit.
 void error(char *fmt, ...) {
     va_list ap;
@@ -85,6 +88,8 @@ static Token *new_token(TokenKind kind, char *start, char *end) {
     tok->kind = kind;
     tok->loc = start;
     tok->len = end - start;
+    tok->at_bol = at_bol;
+    at_bol = false;
     return tok;
 }
 
@@ -374,6 +379,8 @@ static Token *tokenize(char *filename, char *p) {
     Token head = {};
     Token *cur = &head;
 
+    at_bol = true;
+
     while(*p) {
         // Skip line comments.
         if (startswith(p, "//")) {
@@ -389,6 +396,13 @@ static Token *tokenize(char *filename, char *p) {
             if (!q)
                 error_at(p, "unclosed block comment");
             p = q + 2;
+            continue;
+        }
+
+        // Skip newline.
+        if (*p == '\n') {
+            p++;
+            at_bol = true;
             continue;
         }
 
