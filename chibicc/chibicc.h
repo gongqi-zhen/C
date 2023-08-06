ac2d48613a0a2e52d19e96738f950362b87cfdf5
gongqi.zhen <gongqi.zhen@gmail.com>
2023-08-07 00:25:38 +0900

    Add basic "asm" statement

--- chibicc/chibicc.h
+++ chibicc/chibicc.h
@@ -201,6 +201,8 @@ typedef enum {
     ND_VAR,         // Variable
     ND_NUM,         // Integer
     ND_CAST,        // Type cast
+    // ND_MEMZERO,     // Zero-clear a stack variable
+    ND_ASM,         // "asm"
 } NodeKind;
 
 // AST node type
@@ -244,6 +246,9 @@ struct Node {
     // Switch-cases
     Node *case_next;
     Node *default_case;
+
+    // "asm" string literal
+    char *asm_str;
 
     // Variable
     Obj *var;
